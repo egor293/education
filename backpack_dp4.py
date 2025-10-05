3f80c3481df50b021a68995d63a26efb540495c2
egor293 <137516618+egor293@users.noreply.github.com>
2025-10-05 15:35:21 +0300

    Add files via upload

--- backpack_dp4.py
+++ backpack_dp4.py
@@ -11,8 +11,13 @@ ingots = [
     ('gold', 10, 600),
     ('silver', 20, 1000),
     ('bronze', 30, 900),
-    ('diamonds', 3, 1500)
+    ('diamonds', 3, 1500),
+    ('copper', 150, 200)
 ]
+
+def print_table(table):
+    for i in range(len(table)):
+        print(table[i])
 
 
 def greedy_parted(ingots, capacity):
@@ -32,19 +37,14 @@ def greedy_parted(ingots, capacity):
 
     for i in sorted_ingots:
         if capacity > i[1]:
-            backpack['value'] += i[1] * i[2]
-            backpack['items'].append((i[0], i[1], i[1] * i[2]))
+            backpack['value'] += i[2]
+            backpack['items'].append((i[0], i[1], i[2]))
             capacity -= i[1]
         else:
-            backpack['value'] += capacity * i[2]
-            backpack['items'].append((i[0], capacity, capacity * i[2]))
+            backpack['value'] += capacity * (i[2] / i[1])
+            backpack['items'].append((i[0], capacity, capacity * (i[2] / i[1])))
             break
     return backpack
-
-
-
-
-
 
 
 def greedy(values, capacity):
@@ -52,7 +52,7 @@ def greedy(values, capacity):
     cost = sorted(goods.keys(),reverse=True)
     for i in cost:
         if goods[i][0] <= capacity:
-            backpack.append(goods[i][1])
+            backpack.append((goods[i][1], i))
             capacity -= goods[i][0]
             del i
     return backpack
@@ -82,12 +82,36 @@ def dp(values, items, capacity):
     return dp_table[n][capacity], backpack
 
 
+def dp_substring(word_1, word_2):
+    substing = ''
+    substring_length = 0
+    height = len(word_1)
+    width = len(word_2)
+    dp_table = [[0 for _ in range(height + 1)] for _ in range(width + 1)]
+    for i in range(1, width):
+        for j in range(1, height) :
+            if word_1[j] == word_2[i]:
+                dp_table[i][j] = dp_table[i - 1][j - 1] + 1
+                substing += word_1[i]
+
+    for i in dp_table:
+        substring_length = max(substring_length, max(i))
+    print_table(dp_table)
+    return substring_length, substing
+    
+
+
+
+
 parted_res = greedy_parted(ingots, 25)
 greedy_res = greedy(goods, 4)
 dp_res = dp(list(goods.keys()), list(goods.values()), 4)
+dp_substring_res = dp_substring(list('fishfish'), list('hish'))
+
 print('\n\n')
 print(f'Greedy: {greedy_res}', end = '\n\n')
 print(f'Dp: {dp_res}', end = '\n\n')
+print(f'Dp_substring: {dp_substring_res}', end = '\n\n')
 print(f'Parted: {parted_res}', end = '\n\n')
 
 
@@ -108,3 +132,24 @@ print(f'Parted: {parted_res}', end = '\n\n')
 #                 dp[i][w] = dp[i - 1][w]
     
 #     return dp[i][w], dp, backpack
+
+
+
+# LEETCODE 718
+# class Solution:
+#     def findLength(self, nums1: List[int], nums2: List[int]) -> int:
+#         answer = 0
+#         height = len(nums1)
+#         width = len(nums2)
+#         dp_table = [[0 for _ in range(height + 1)] for _ in range(width + 1)]
+#         for i in range(width):
+#             for j in range(height):
+#                 if nums1[j] == nums2[i]:
+#                     if i == 0 or j == 0:
+#                         dp_table[i][j] = 1
+#                         continue
+#                     dp_table[i][j] = dp_table[i - 1][j - 1] + 1
+
+#         for i in dp_table:
+#             answer = max(answer, max(i))
+#         return answer
