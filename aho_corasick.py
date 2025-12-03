e5df4060acc108bc0b968ec7a30ef8ff68d9f2c7
egor293 <137516618+egor293@users.noreply.github.com>
2025-12-03 15:28:14 +0300

    Update aho_corasick.py

--- aho_corasick.py
+++ aho_corasick.py
@@ -1,29 +1,22 @@
 def build_trie(patterns: list[str]):
-    # edges[u][ch] = v — из узла u по символу ch переход в v
-    edges = [{} ]  # edges[0] — корень
-    # output[u] — список номеров шаблонов, которые заканчиваются в узле u
+    edges = [{}]  
     output = [[]]
+    nodes = 1 
     
-    node_cnt = 1  # следующий свободный номер узла
+    for ptI, pt in enumerate(patterns):
+        i = 0  
+        for ch in pt:
+            if ch not in edges[i]:
+                edges[i][ch] = nodes
+                edges.append({})      
+                output.append([])     
+                nodes += 1
+            i = edges[i][ch]  
+        output[i].append(ptI)
     
-    for pat_id, pat in enumerate(patterns):
-        u = 0  # начинаем с корня
-        for ch in pat:
-            if ch not in edges[u]:
-                # создаём новый узел
-                edges[u][ch] = node_cnt
-                edges.append({})      # новый словарь переходов
-                output.append([])     # новый список выводов
-                node_cnt += 1
-            u = edges[u][ch]  # переходим дальше
-        # по окончании шаблона записываем его номер в output
-        output[u].append(pat_id)
-    
-    return edges, output, node_cnt
+    return edges, output
 
 
-# Тест
-edges, output, total_nodes = build_trie(["ababac", "bababab", "ab", "bab"])
-print("Всего узлов:", total_nodes)
-print("edges:", [dict(e) for e in edges])  # для красивого вывода
+edges, output = build_trie(["ababac", "bababab", "ab", "bab"])
+print("edges:", edges)  
 print("output:", output)
