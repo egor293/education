7cde4021bcde26cb4c10568caeb47a3ccce781b9
egor293 <137516618+egor293@users.noreply.github.com>
2026-01-11 12:49:29 +0300

    Update DSU.py

--- DSU.py
+++ DSU.py
@@ -54,5 +54,33 @@ print(find(0) == find(2))
 print(find(0) == find(4)) 
 
 
+class DSU:
+    def __init__(self, n):
+        self.parent = list(range(n)) # Каждый элемент - сам себе родитель
+        self.rank = [0] * n # Для оптимизации union by rank
+
+    def find(self, i):
+        if self.parent[i] == i:
+            return i
+        self.parent[i] = self.find(self.parent[i]) # Сжатие пути
+        return self.parent[i]
+
+    def union(self, i, j):
+        root_i = self.find(i)
+        root_j = self.find(j)
+        if root_i != root_j:
+            # Union by rank
+            if self.rank[root_i] < self.rank[root_j]:
+                self.parent[root_i] = root_j
+            elif self.rank[root_i] > self.rank[root_j]:
+                self.parent[root_j] = root_i
+            else:
+                self.parent[root_j] = root_i
+                self.rank[root_i] += 1
+            return True # Успешно объединены
+        return False # Уже в одном множестве
+
+
+
 
 # сверхъёлка
